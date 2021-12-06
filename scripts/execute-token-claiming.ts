9b6396deeae56fdaeb0ef5de5cef69acfef4ee6d
PJColombo <paulo.colombo@pm.me>
2021-12-06 18:29:48 +0100

    Add token claiming script

---
+++ scripts/execute-token-claiming.ts
@@ -0,0 +1,49 @@
+import { ethers } from "hardhat";
+import ora from "ora";
+import { EVMcrispr } from "@1hive/evmcrispr";
+import { claimTokens } from "./actions";
+import { GARDEN_DAO_ADDRESS, HATCH_TOKEN_ADDRESS } from "../commons-config";
+import {
+  getAppContract,
+  getTokenHolders,
+  MAX_TX_GAS_LIMIT,
+  MAX_TX_GAS_PRICE,
+} from "../test/helpers";
+
+let spinner = ora();
+
+async function main() {
+  const signer = (await ethers.getSigners())[0];
+
+  spinner = spinner.start(
+    `Create evmcrispr instance for DAO: ${GARDEN_DAO_ADDRESS}`
+  );
+
+  const commonsEVMcrispr = await EVMcrispr.create(GARDEN_DAO_ADDRESS, signer);
+
+  spinner.succeed();
+
+  const commonsMigrationTools = getAppContract(
+    "migration-tools.open:0",
+    commonsEVMcrispr
+  );
+  const tokenHolders = await getTokenHolders(HATCH_TOKEN_ADDRESS);
+  const tokenHolderAddresses = tokenHolders.map((holder) =>
+    holder.address.toLowerCase()
+  );
+
+  await claimTokens(
+    commonsMigrationTools.claimForMany,
+    tokenHolderAddresses,
+    { gasLimit: MAX_TX_GAS_LIMIT, gasPrice: MAX_TX_GAS_PRICE },
+    console.log
+  );
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    spinner.fail();
+    console.error(error);
+    process.exit(1);
+  });
